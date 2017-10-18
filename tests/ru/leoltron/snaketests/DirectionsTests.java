3835a8f124c485112e0fe963cfe5ac9cead6ff07
Leoltron <leoole5@gmail.com>
2017-10-18 11:43:55 +0500

    Fixed DirectionsTests

--- tests/ru/leoltron/snaketests/DirectionsTests.java
+++ tests/ru/leoltron/snaketests/DirectionsTests.java
@@ -1,34 +1,33 @@
-//package ru.leoltron.snaketests;
-//
-//import lombok.val;
-//import org.junit.Assert;
-//import org.junit.Test;
-//import ru.leoltron.snake.game.Direction;
-//
-//import java.awt.*;
-//
-//public class DirectionsTests extends Assert {
-//    @Test
-//    public void testTranslationUp() {
-//        val point = new Point(5, 5);
-//        assertEquals(Direction.UP.translatePoint(point), new Point(5, 4));
-//    }
-//
-//    @Test
-//    public void testTranslationDown() {
-//        val point = new Point(5, 5);
-//        assertEquals(Direction.DOWN.translatePoint(point), new Point(5, 6));
-//    }
-//
-//    @Test
-//    public void testTranslationLeft() {
-//        val point = new Point(5, 5);
-//        assertEquals(Direction.LEFT.translatePoint(point), new Point(4, 5));
-//    }
-//
-//    @Test
-//    public void testTranslationRight() {
-//        val point = new Point(5, 5);
-//        assertEquals(Direction.RIGHT.translatePoint(point), new Point(6, 5));
-//    }
-//}
+package ru.leoltron.snaketests;
+
+import lombok.val;
+import org.junit.Assert;
+import org.junit.Test;
+import ru.leoltron.snake.game.Direction;
+import ru.leoltron.snake.util.GamePoint;
+
+public class DirectionsTests extends Assert {
+    @Test
+    public void testTranslationUp() {
+        val point = new GamePoint(5, 5);
+        assertEquals(point.translated(Direction.UP), new GamePoint(5, 4));
+    }
+
+    @Test
+    public void testTranslationDown() {
+        val point = new GamePoint(5, 5);
+        assertEquals(point.translated(Direction.DOWN), new GamePoint(5, 6));
+    }
+
+    @Test
+    public void testTranslationLeft() {
+        val point = new GamePoint(5, 5);
+        assertEquals(point.translated(Direction.LEFT), new GamePoint(4, 5));
+    }
+
+    @Test
+    public void testTranslationRight() {
+        val point = new GamePoint(5, 5);
+        assertEquals(point.translated(Direction.RIGHT), new GamePoint(6, 5));
+    }
+}
