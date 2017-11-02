99a2abf7116b6dd78d696d673d17b6429876a6ed
Danil <jyorry@gmail.com>
2017-11-03 00:15:22 +0500

    Added class EventDispatcher

---
+++ src/ru/leoltron/snake/game/entity/EventDispatcher.java
@@ -0,0 +1,18 @@
+package ru.leoltron.snake.game.entity;
+
+
+public final class Observer {
+
+    private Observer() {}
+
+    public void addListener()
+    {
+
+    }
+
+    public void sendMessege()
+    {
+        
+    }
+
+}
